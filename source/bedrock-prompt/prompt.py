cc1d38b7e573715d8be7d7266e11a836ca3c3ab9
Andre Cavalcante <andrecvnt@gmail.com>
2025-02-05 14:41:01 -0300

    Fix typo

--- source/bedrock-prompt/prompt.py
+++ source/bedrock-prompt/prompt.py
@@ -6,48 +6,51 @@ import boto3
 import json
 import sys
 
+
 def load_prompt():
-    with open('../bedrock-prompt/prompt.txt', 'r') as file:
+    with open("../bedrock-prompt/prompt.txt", "r") as file:
         return file.read()
+
 
 def concatenate_logs():
     parent_dir = "."
     concatenated_logs = ""
     log_files = ["scp.log", "tf.log"]
-    
+
     for log_file in log_files:
         file_path = os.path.join(parent_dir, log_file)
         if os.path.exists(file_path):
-            with open(file_path, 'r') as file:
+            with open(file_path, "r") as file:
                 concatenated_logs += file.read() + "\n\n"
         else:
             print(f"Warning: {log_file} not found in the parent directory.")
             sys.exit(1)
-    
-    return concatenated_logs
-def query_bedrock(prompt):
-    bedrock = boto3.client(service_name='bedrock-runtime')
-    
-    body = json.dumps({
-        "anthropic_version": "bedrock-2023-05-31",
-        "max_tokens": 4096,
-        "messages": [
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        "temperature": 0
-    })
-    
-    modelId = 'anthropic.claude-3-5-sonnet-20240620-v1:0'
-    accept = 'application/json'
-    contentType = 'application/json'
 
-    response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
-    response_body = json.loads(response.get('body').read())
-    
-    return response_body['content'][0]['text']
+    return concatenated_logs
+
+
+def query_bedrock(prompt):
+    bedrock = boto3.client(service_name="bedrock-runtime")
+
+    body = json.dumps(
+        {
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": 4096,
+            "messages": [{"role": "user", "content": prompt}],
+            "temperature": 0,
+        }
+    )
+
+    modelId = "anthropic.claude-3-5-sonnet-20240620-v1:0"
+    accept = "application/json"
+    contentType = "application/json"
+
+    response = bedrock.invoke_model(
+        body=body, modelId=modelId, accept=accept, contentType=contentType
+    )
+    response_body = json.loads(response.get("body").read())
+
+    return response_body["content"][0]["text"]
 
 
 # Main execution
@@ -60,11 +63,11 @@ prompt += "Assistant:"
 response = query_bedrock(prompt)
 
 print("####################################")
-print("######## Gen IA Log Summary  #######")
+print("######## Gen AI Log Summary  #######")
 print("####################################")
 print()
 print()
 print(response)
 
-with open('summary.txt', 'w') as file:
+with open("summary.txt", "w") as file:
     file.write(response)
